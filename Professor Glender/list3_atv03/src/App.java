4cb592556b308e27ebfe0dc4bf076c90e0e54003
Sudré <you@example.com>
2022-10-26 10:23:03 -0300

    Commit 26/10

---
+++ Professor Glender/list3_atv03/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int inteiros[];
+        inteiros = new int[9];
+
+        System.out.println("Digite 9 numeros inteiros: ");
+        for (int i=0; i<inteiros.lenght; i++){}
+
+
+
+
+
+    }
+}
