c64ba01a4cc4dc9746b40096a5feefd5a34f0b1f
Sudré <you@example.com>
2022-12-12 20:53:42 -0300

    Commit 12/12

--- Professor Montanha/EncomendaDoSistema/src/App.java
+++ Professor Montanha/EncomendaDoSistema/src/App.java
@@ -1,10 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         DadosDesenvolvedor dadosDesenvolvedor = new DadosDesenvolvedor();
+        
         System.out.println("Nome do profissional: " + dadosDesenvolvedor.Nome);
         System.out.println("Salario desejado: " + dadosDesenvolvedor.SalarioMensalDesejado);
         System.out.println("Quantidade de horas no dia: " + dadosDesenvolvedor.HorasDia);
-        System.out.println("Valor hora: " + dadosDesenvolvedor.Valorhora);
+        System.out.println("Valor hora: " + Math.round(dadosDesenvolvedor.HorasTrabalhadasMes()));
 
         //1 3(1 = 8, 2 = 12, 3 = 16)
         ItemSistema itemEntradasSaidas = new ItemSistema();
@@ -21,8 +22,27 @@ public class App {
         itemGravarDados.nome = "Gravação de dados";
         itemGravarDados.complexidadeGravarDados = 2;
 
-        double valortotal = dadosDesenvolvedor.HorasTrabalhadasMes() * (itemEntradasSaidas.CalculaEsforçoEntradaSaidaDados() + itemConsultaInfos.CalculaEsforçoConsultaDados() + itemGravarDados.CalculaEsforçoGravarDados());
+        double valortotal = (itemEntradasSaidas.CalculaEsforçoEntradaSaidaDados() + itemConsultaInfos.CalculaEsforçoConsultaDados() + itemGravarDados.CalculaEsforçoGravarDados());
+        double valorsistema = valortotal * dadosDesenvolvedor.Valorhora;
+        double porcentagemAdd = valorsistema * 0.03;
 
-    
+
+        System.out.println(" ");
+        System.out.println("Item de sistema: " + itemEntradasSaidas.nome);
+        System.out.println("Complexidade do Item de Sistema: " + itemEntradasSaidas.complexidadeEntradaSaidaDados);
+        System.out.println("Esforço do item de sistema: " + itemEntradasSaidas.CalculaEsforçoEntradaSaidaDados() + "hrs");
+
+        System.out.println(" ");
+        System.out.println("Item de sistema: " + itemConsultaInfos.nome);
+        System.out.println("Complexidade do Item de Sistema: " + itemConsultaInfos.complexidadeConsultaDados);
+        System.out.println("Esforço do item de sistema: " + itemConsultaInfos.CalculaEsforçoConsultaDados() + "hrs");
+
+        System.out.println(" ");
+        System.out.println("Item de sistema: " + itemGravarDados.nome);
+        System.out.println("Complexidade do Item de Sistema: " + itemGravarDados.complexidadeGravarDados);
+        System.out.println("Esforço do item de sistema: " + itemGravarDados.CalculaEsforçoGravarDados() + "hrs");
+
+        System.out.println(" ");
+        System.out.println("Valor total do sistema: " + valorsistema + porcentagemAdd);
     }
 }
--- Professor Montanha/EncomendaDoSistema/src/DadosDesenvolvedor.java
+++ Professor Montanha/EncomendaDoSistema/src/DadosDesenvolvedor.java
@@ -2,7 +2,7 @@ public class DadosDesenvolvedor {
     public String Nome = "João Paulo";
     public double HorasDia = 7;
     public double SalarioMensalDesejado = 4000;
-    public double Valorhora = 20;
+    public double Valorhora = 60;
 
     public double HorasTrabalhadasMes() {
         return SalarioMensalDesejado/(20*HorasDia);
